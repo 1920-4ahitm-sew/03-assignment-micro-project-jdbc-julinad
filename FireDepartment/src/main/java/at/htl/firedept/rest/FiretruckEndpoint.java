173d9392b4e7ab3ecec805240c1723e4b6613a2a
julinad <juli.naderer@gmail.com>
2019-10-22 13:47:14 +0200

    added class FiretruckEndpoint

---
+++ FireDepartment/src/main/java/at/htl/firedept/rest/FiretruckEndpoint.java
@@ -0,0 +1,12 @@
+package at.htl.firedept.rest;
+
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("firedept")
+@Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+public class FiretruckEndpoint {
+
+}
