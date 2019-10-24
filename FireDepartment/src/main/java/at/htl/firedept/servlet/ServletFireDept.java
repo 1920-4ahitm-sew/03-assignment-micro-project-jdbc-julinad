7dae76d62d050f0b67392b3c74553c4d5f377fe2
julinad <juli.naderer@gmail.com>
2019-10-24 09:34:58 +0200

    added doGet method

--- FireDepartment/src/main/java/at/htl/firedept/servlet/ServletFireDept.java
+++ FireDepartment/src/main/java/at/htl/firedept/servlet/ServletFireDept.java
@@ -1,4 +1,14 @@
 package at.htl.firedept.servlet;
 
-public class ServletFireDept {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("servlet")
+public class ServletFireDept extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+
+    }
 }
