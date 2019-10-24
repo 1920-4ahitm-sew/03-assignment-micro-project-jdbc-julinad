9877b1c88dcca1947cd9a7cc407a8ea668a14f77
julinad <juli.naderer@gmail.com>
2019-10-24 09:30:34 +0200

    deleted jdbc database connection methods

--- FireDepartment/src/main/java/at/htl/firedept/rest/FiretruckEndpoint.java
+++ FireDepartment/src/main/java/at/htl/firedept/rest/FiretruckEndpoint.java
@@ -4,6 +4,9 @@ package at.htl.firedept.rest;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Path("firedept")
 @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
